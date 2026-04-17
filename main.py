a2bbda5ce8e130a995208d2e591262191e60e90d
kaiquejw <kaiquecapt@gmail.com>
2026-04-16 22:31:45 -0300

    teste

--- main.py
+++ main.py
@@ -14,21 +14,15 @@ API_HASH = os.environ.get('TELEGRAM_API_HASH')
 CONTAS = [
 
 
-        #  DOBRA 19:00 🟣 -1002742703600
-        {
-            "nome": "Vanessa", 
-            "secret_name": "SESSION_VANESSA",
-            "chat_id": -1002742703600,
-            "msg": "Vanessa X narizinho r4"
-        },
+
 
         #  20h00  Grupo da senha DOBRA Flórida Paulista -1002443109385
-        #{
-        #    "nome": "Paula", 
-        #    "secret_name": "SESSION_PAULA",
-        #    "chat_id": -1002443109385,
-        #    "msg": "Paula x Carlos raio 3 cela 8"
-        #},
+        {
+            "nome": "Paula", 
+            "secret_name": "SESSION_PAULA",
+            "chat_id": -1002443109385,
+            "msg": "Paula x Carlos raio 3 cela 8"
+        },
 
         #  20h45  SENHA PREF 🚨 -1003049291964
         #{
@@ -41,7 +35,7 @@ CONTAS = [
 ]
 
 # ⚠️ AJUSTE AQUI PARA O DIA DA SENHA ⚠️
-HORA_ALVO = 19
+HORA_ALVO = 20
 MINUTO_ALVO = 00
 
 async def sniper_individual(conta, alvo):
