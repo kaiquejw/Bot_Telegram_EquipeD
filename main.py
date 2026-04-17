24c2ef35a640d24e9aa259ae7c59acae156799fc
kaiquejw <kaiquecapt@gmail.com>
2026-04-16 23:07:35 -0300

    teste

--- main.py
+++ main.py
@@ -13,30 +13,19 @@ API_HASH = os.environ.get('TELEGRAM_API_HASH')
 # ⚠️ MUDANÇA 1: JAQUELINE EM PRIMEIRO (PRIORIDADE MÁXIMA) ⚠️
 CONTAS = [
 
-
-
-
-        #  20h00  Grupo da senha DOBRA Flórida Paulista -1002443109385
-        {
-            "nome": "Paula", 
-            "secret_name": "SESSION_PAULA",
-            "chat_id": -1002443109385,
-            "msg": "Paula x Carlos raio 3 cela 8"
-        },
-
         #  20h45  SENHA PREF 🚨 -1003049291964
-        #{
-        #    "nome": "Anne", 
-        #    "secret_name": "SESSION_ANNE",
-        #    "chat_id": -1003049291964,
-        #    "msg": "Anny JB R2"
-        #},        
+        {
+            "nome": "Anne", 
+            "secret_name": "SESSION_ANNE",
+            "chat_id": -1003049291964,
+            "msg": "Anny JB R2"
+        },        
 
 ]
 
 # ⚠️ AJUSTE AQUI PARA O DIA DA SENHA ⚠️
 HORA_ALVO = 20
-MINUTO_ALVO = 00
+MINUTO_ALVO = 45
 
 async def sniper_individual(conta, alvo):
     """Função otimizada para VELOCIDADE MÁXIMA (Modo Turbo)"""
