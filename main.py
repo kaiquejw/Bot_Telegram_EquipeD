9ceaf0d61d14a7a7fbd4cc747102320c70279317
kaiquejw <kaiquecapt@gmail.com>
2026-04-29 19:20:02 -0300

    Teste de quarta

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ CONTAS = [
 
 # ⚠️ AJUSTE AQUI PARA O DIA DA SENHA ⚠️
 HORA_ALVO = 16
-MINUTO_ALVO = 19
+MINUTO_ALVO = 21
 
 async def sniper_individual(conta, alvo):
     """Função otimizada para VELOCIDADE MÁXIMA (Modo Turbo)"""
