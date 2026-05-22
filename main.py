78c4362424bcd53f07ecd9d0c820c9d50a98f265
kaiquejw <kaiquecapt@gmail.com>
2026-05-21 23:20:04 -0300

    Prod Rafa

--- main.py
+++ main.py
@@ -18,15 +18,15 @@ CONTAS = [
     {
         "nome": "Katia", 
         "secret_name": "SESSION_KATIA",
-        "chat_id": -5117474448,
+        "chat_id": -1003010614751,
         "msg": "Katia pantanal r2 laudo"
     },
 
 ]
 
 # ⚠️ AJUSTE AQUI PARA O DIA DA SENHA ⚠️
-HORA_ALVO = 20
-MINUTO_ALVO = 18
+HORA_ALVO = 21
+MINUTO_ALVO = 00
 
 async def sniper_individual(conta, alvo):
     """Função otimizada para VELOCIDADE MÁXIMA (Modo Turbo)"""
@@ -111,7 +111,7 @@ async def sniper_individual(conta, alvo):
 
 async def main():
     agora = datetime.datetime.now()
-    alvo = agora.replace(hour=HORA_ALVO, minute=MINUTO_ALVO, second=0, microsecond=300000)
+    alvo = agora.replace(hour=HORA_ALVO, minute=MINUTO_ALVO, second=0, microsecond=0)
     
     print(f"🔥 INICIANDO MODO TURBO ({len(CONTAS)} contas)")
     print(f"🎯 Alvo: {alvo.strftime('%H:%M:%S')}")
