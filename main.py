c2ec03b3811ef2189bb853e445d6711fbfca7d9a
kaiquejw <kaiquecapt@gmail.com>
2026-05-28 23:01:51 -0300

    Teste 1

--- main.py
+++ main.py
@@ -14,28 +14,19 @@ API_HASH = os.environ.get('TELEGRAM_API_HASH')
 CONTAS = [
 
 
-
-        # 20h00 Grupo da senha DOBRA Flórida Paulista -1002443109385
-    {
-        "nome": "Thaina", 
-        "secret_name": "SESSION_THAINA",
-        "chat_id": -1002443109385,
-        "msg": "Thaina X Daniel R2"
-    },
-
         # 20h30 SENHA NORMAL 🚨 -1002704903786
-    #{
-    #    "nome": "Pit", 
-    #    "secret_name": "SESSION_PIT",
-    #    "chat_id": -1002704903786,
-    #    "msg": "Pit x poeira Raio 6"
-    #},
+    {
+        "nome": "Pit", 
+        "secret_name": "SESSION_PIT",
+        "chat_id": -1002704903786,
+        "msg": "Pit x poeira Raio 6"
+    },
 
 ]
 
 # ⚠️ AJUSTE AQUI PARA O DIA DA SENHA ⚠️
 HORA_ALVO = 20
-MINUTO_ALVO = 00
+MINUTO_ALVO = 30
 
 async def sniper_individual(conta, alvo):
     """Função otimizada para VELOCIDADE MÁXIMA (Modo Turbo)"""
@@ -120,7 +111,7 @@ async def sniper_individual(conta, alvo):
 
 async def main():
     agora = datetime.datetime.now()
-    alvo = agora.replace(hour=HORA_ALVO, minute=MINUTO_ALVO, second=0, microsecond=0)
+    alvo = agora.replace(hour=HORA_ALVO, minute=MINUTO_ALVO, second=1, microsecond=0)
     
     print(f"🔥 INICIANDO MODO TURBO ({len(CONTAS)} contas)")
     print(f"🎯 Alvo: {alvo.strftime('%H:%M:%S')}")
